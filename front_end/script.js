03449bf90bb70e8f03a47c4c60e9251bb75010ae
noahuw08 <vk3nguye@uwaterloo.ca>
2023-02-21 02:06:06 -0500

    added artworks, artists, exhibitions, rooms sections

--- front_end/script.js
+++ front_end/script.js
@@ -1,56 +1,164 @@
-// Get the gallery data from the API endpoint
-fetch('https://mygalleryapi.com/gallery')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through the gallery data and create a card for each artwork
-    const cardDeck = document.querySelector('.card-deck');
-    data.forEach(artwork => {
-      const card = document.createElement('div');
-      card.classList.add('card');
+// Function to display artworks
+function displayArtworks() {
+  // Get the artworks section element
+  let artworksSection = document.getElementById("artworks-section");
 
-      const image = document.createElement('img');
-      image.src = artwork.image_url;
-      image.classList.add('card-img-top');
-      card.appendChild(image);
+  // Clear the section
+  artworksSection.innerHTML = "";
 
-      const cardBody = document.createElement('div');
-      cardBody.classList.add('card-body');
-      card.appendChild(cardBody);
+  // Loop through the artworks array and create a card for each artwork
+  artworks.forEach(function(artwork) {
+    // Create a card element
+    let card = document.createElement("div");
+    card.classList.add("card");
 
-      const title = document.createElement('h5');
-      title.classList.add('card-title');
-      title.textContent = artwork.title;
-      cardBody.appendChild(title);
+    // Create an image element and set the source to the artwork image URL
+    let img = document.createElement("img");
+    img.src = artwork.imageUrl;
+    img.alt = artwork.title;
 
-      const artist = document.createElement('p');
-      artist.classList.add('card-text');
-      artist.textContent = 'By ' + artwork.artist;
-      cardBody.appendChild(artist);
+    // Create a heading element and set the text to the artwork title
+    let heading = document.createElement("h3");
+    heading.textContent = artwork.title;
 
-      const price = document.createElement('p');
-      price.classList.add('card-text');
-      price.textContent = 'Price: $' + artwork.price;
-      cardBody.appendChild(price);
+    // Create a paragraph element and set the text to the artwork artist name and date
+    let artistAndDate = document.createElement("p");
+    artistAndDate.textContent = `${artwork.artist} (${artwork.date})`;
 
-      const cardFooter = document.createElement('div');
-      cardFooter.classList.add('card-footer');
-      card.appendChild(cardFooter);
+    // Create a paragraph element and set the text to the artwork medium and location
+    let mediumAndLocation = document.createElement("p");
+    mediumAndLocation.textContent = `${artwork.medium} - ${artwork.location}`;
 
-      const button = document.createElement('button');
-      button.classList.add('btn', 'btn-primary');
-      button.textContent = 'Add to Cart';
-      cardFooter.appendChild(button);
+    // Append the image, heading, artistAndDate, and mediumAndLocation elements to the card
+    card.appendChild(img);
+    card.appendChild(heading);
+    card.appendChild(artistAndDate);
+    card.appendChild(mediumAndLocation);
 
-      cardDeck.appendChild(card);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching gallery data:', error);
+    // Append the card to the artworks section
+    artworksSection.appendChild(card);
   });
+}
 
-// Handle the Add to Cart button click event
-const cartButton = document.querySelector('#cart-button');
-cartButton.addEventListener('click', event => {
-  event.preventDefault();
-  alert('This feature is not yet implemented.');
+// Function to display artists
+function displayArtists() {
+  // Get the artists section element
+  let artistsSection = document.getElementById("artists-section");
+
+  // Clear the section
+  artistsSection.innerHTML = "";
+
+  // Loop through the artists array and create a card for each artist
+  artists.forEach(function(artist) {
+    // Create a card element
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    // Create an image element and set the source to the artist image URL
+    let img = document.createElement("img");
+    img.src = artist.imageUrl;
+    img.alt = artist.name;
+
+    // Create a heading element and set the text to the artist name
+    let heading = document.createElement("h3");
+    heading.textContent = artist.name;
+
+    // Create a paragraph element and set the text to the artist nationality and birth/death years
+    let nationalityAndYears = document.createElement("p");
+    nationalityAndYears.textContent = `${artist.nationality} (${artist.birthYear}-${artist.deathYear})`;
+
+    // Append the image, heading, and nationalityAndYears elements to the card
+    card.appendChild(img);
+    card.appendChild(heading);
+    card.appendChild(nationalityAndYears);
+
+    // Append the card to the artists section
+    artistsSection.appendChild(card);
+  });
+}
+
+function displayExhibitions() {
+  // Get the container where we will add the exhibition information
+  const container = document.querySelector(".exhibitions-container");
+
+  // Loop through the exhibitions array and create an HTML element for each one
+  for (let i = 0; i < exhibitions.length; i++) {
+    // Create a div element to hold the exhibition information
+    const exhibitionDiv = document.createElement("div");
+    exhibitionDiv.classList.add("exhibition");
+
+    // Create a heading element and set the text to the exhibition title
+    const title = document.createElement("h2");
+    title.textContent = exhibitions[i].title;
+    exhibitionDiv.appendChild(title);
+
+    // Create an image element and set the source to the exhibition image
+    const image = document.createElement("img");
+    image.src = exhibitions[i].image;
+    exhibitionDiv.appendChild(image);
+
+    // Create a paragraph element and set the text to the exhibition description
+    const description = document.createElement("p");
+    description.textContent = exhibitions[i].description;
+    exhibitionDiv.appendChild(description);
+
+    // Create a button element to view the exhibition details and set the text to "View Details"
+    const button = document.createElement("button");
+    button.textContent = "View Details";
+    exhibitionDiv.appendChild(button);
+
+    // Add an event listener to the button to display the exhibition details when clicked
+    button.addEventListener("click", function () {
+      displayExhibitionDetails(i);
+    });
+
+    // Add the exhibition div to the container
+    container.appendChild(exhibitionDiv);
+  }
+}
+
+
+// Add event listeners to navigation links
+document.getElementById("nav-artworks").addEventListener("click", function() {
+  toggleActive("nav-artworks");
+  showContent("artworks");
 });
+document.getElementById("nav-artists").addEventListener("click", function() {
+  toggleActive("nav-artists");
+  showContent("artists");
+});
+document.getElementById("nav-exhibitions").addEventListener("click", function() {
+  toggleActive("nav-exhibitions");
+  showContent("exhibitions");
+});
+document.getElementById("nav-rooms").addEventListener("click", function() {
+  toggleActive("nav-rooms");
+  showContent("rooms");
+});
+
+// Toggle active class on navigation links
+function toggleActive(id) {
+  // Remove active class from all links
+  var links = document.getElementsByClassName("nav-link");
+  for (var i = 0; i < links.length; i++) {
+    links[i].classList.remove("active");
+  }
+  // Add active class to selected link
+  document.getElementById(id).classList.add("active");
+}
+
+// Show content section
+function showContent(id) {
+  // Hide all content sections
+  var sections = document.getElementsByClassName("content-section");
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].style.display = "none";
+  }
+  // Show selected section
+  document.getElementById(id).style.display = "block";
+}
+
+// Initialize page by showing default content
+toggleActive("nav-artworks");
+showContent("artworks");
+
